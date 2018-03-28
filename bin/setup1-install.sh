b4bfc2a22828f61dcabff6c3df1952a01bc51a0e
ytani01 <yoichi@tanibayashi.jp>
2018-03-28 22:28:55 +0900

    setup

--- bin/setup1-install.sh
+++ bin/setup1-install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh -x
 #
-MYNAME=tanibayashi
+JSON_FNAME=tanibayashi
 ENVNAME=env
 ENVDIR=${HOME}/${ENVNAME}
 
+cd
+
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install python3-dev python3-venv portaudio19-dev libffi-dev libssl-dev
+sudo apt-get -y install python3-dev python3-venv
 
-cd
 python3 -m venv ${ENVNAME}
 ${ENVDIR}/bin/python -m pip install --upgrade pip setuptools wheel
 . ${ENVDIR}/bin/activate
 
+sudo apt-get -y install portaudio19-dev libffi-dev libssl-dev
 python -m pip install --upgrade google-assistant-library
 python -m pip install --upgrade google-assistant-sdk[samples]
+
 python -m pip install --upgrade google-auth-oauthlib[tool]
 
-google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless --client-secrets /boot/${MYNAME}.json
+google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless --client-secrets /boot/${JSON_FNAME}.json
