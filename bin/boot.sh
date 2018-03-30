d325dfacd8710ef72ac22f7b328ac4b051bc8c4d
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-03-31 01:05:05 +0900

    update

---
+++ bin/boot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BINDIR=${HOME}/bin
+
+if [ -x ${BINDIR}/AQM1602A-time-ipaddr.py ]; then
+	${BINDIR}/AQM1602A-time-ipaddr.py > /dev/null 2>&1 &
+fi
+
+if [ -x ${BINDIR}/FabLabKannai-GoogleAssistant.sh ]; then
+	sleep 10
+	${BINDIR}/FabLabKannai-GoogleAssistant.sh &
+else
+	${BINDIR}/boot-speakipaddr.sh repeat > /dev/null 2>&1
+fi
