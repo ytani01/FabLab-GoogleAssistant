2f073b76bdaa98c4cbddbe41e22cec920767b9a4
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2018-04-07 16:09:28 +0900

    boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -5,19 +5,33 @@ date
 
 BINDIR=${HOME}/bin
 
-if [ -x ${BINDIR}/AQM1602A-time-ipaddr.py ]; then
-	${BINDIR}/AQM1602A-time-ipaddr.py > /dev/null 2>&1 &
+LCD=""
+
+CMD=${BINDIR}/thermometer-ipaddr-time.py
+I2C_DETECT=`/usr/sbin/i2cdetect -y 1 | grep 3e | sed 's/:.*$//'`
+echo "I2C_DETECT=${I2C_DETECT}"
+if [ -x ${CMD} ]; then
+	if [ X${I2C_DETECT} != X ]; then
+		${CMD} > /dev/null 2>&1 &
+		LCD="ON"
+	fi
 fi
 
-if [ -x ${BINDIR}/FabLabKannai-GoogleAssistant.sh ]; then
-	sleep 10
-	${BINDIR}/FabLabKannai-GoogleAssistant.sh &
-else
-	while ! (aplay -l | grep seeed); do
-	    echo -n '.'
-	    sleep 1
-	done
-	${BINDIR}/boot-speakipaddr.sh repeat
+while ! (aplay -l | grep seeed); do
+	echo -n "."
+	sleep 1
+done
+
+if [ X${LCD} = X ]; then
+	CMD="${BINDIR}/boot-speakipaddr.sh"
+	if [ -x ${CMD} ]; then
+		${CMD}
+	fi
+fi
+
+CMD="${BINDIR}/FabLabKannai-GoogleAssistant.sh"
+if [ -x ${CMD} ]; then
+	${CMD} &
 fi
 
 echo -n '========== '
